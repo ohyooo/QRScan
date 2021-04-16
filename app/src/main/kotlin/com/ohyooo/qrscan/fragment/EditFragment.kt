eef69c274d53ebbb7176d850097c9727d35fb08d
ohyooo <i@ohyooo.com>
2021-04-16 16:06:30 +0800

    update

--- app/src/main/kotlin/com/ohyooo/qrscan/fragment/EditFragment.kt
+++ app/src/main/kotlin/com/ohyooo/qrscan/fragment/EditFragment.kt
@@ -10,9 +10,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
@@ -39,10 +36,9 @@ class EditFragment : Fragment(), HasTitle {
 
     @Composable
     fun EditText() {
-        var text by remember { vm.result }
         TextField(
-            value = text,
-            onValueChange = { text = it },
+            value = vm.result.value,
+            onValueChange = { vm.result.value = it },
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
