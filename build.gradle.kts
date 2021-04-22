6c420fbfc6ff3201983556b5fc3ae49b47de74ce
ohyooo <i@ohyooo.com>
2021-04-22 11:34:25 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
-        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.15")
+        classpath(Libs.Plugin.AGP)
+        classpath(Libs.Plugin.KGP)
+        classpath(Libs.Plugin.PGP)
     }
 }
 
