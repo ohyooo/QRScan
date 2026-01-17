7b15719a31cc8f1c1ecd07cbb9198fa70fc7175b
i <i@ohyooo.com>
2026-01-17 21:47:45 +0800

    fix history

--- app/src/main/kotlin/com/ohyooo/qrscan/compose/HistoryUI.kt
+++ app/src/main/kotlin/com/ohyooo/qrscan/compose/HistoryUI.kt
@@ -1,6 +1,5 @@
 package com.ohyooo.qrscan.compose
 
-import android.content.Context
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -22,24 +22,27 @@ import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withLink
 import androidx.compose.ui.unit.dp
+import com.ohyooo.qrscan.util.KEY_LIST
+import com.ohyooo.qrscan.util.ds
+import kotlinx.serialization.json.Json
 import java.util.regex.Pattern
 
 @Composable
 fun HistoryUI() {
-    LocalContext.current
-    rememberCoroutineScope()
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
 
     val results = remember {
         mutableStateListOf<String>()
     }
 
-    // LaunchedEffect(coroutineScope) {
-    //     context.ds.data.collect {
-    //         val listString = it[KEY_LIST] ?: return@collect
-    //         results.clear()
-    //         results.addAll(Json.decodeFromString(listString))
-    //     }
-    // }
+    LaunchedEffect(coroutineScope) {
+        context.ds.data.collect {
+            val listString = it[KEY_LIST] ?: return@collect
+            results.clear()
+            results.addAll(Json.decodeFromString(listString))
+        }
+    }
 
     LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(results) { result ->
@@ -49,7 +52,7 @@ fun HistoryUI() {
                     .fillMaxWidth()
             ) {
                 val annotatedLinkString = buildAnnotatedString {
-                    val pattern = Pattern.compile("(?:(?:https?|ftp)://)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)")
+                    val pattern = Pattern.compile("(?:(?:https?|ftp)://)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)")
                     val matcher = pattern.matcher(result)
                     val builder = AnnotatedString.Builder(result)
 
--- app/src/main/kotlin/com/ohyooo/qrscan/util/history.kt
+++ app/src/main/kotlin/com/ohyooo/qrscan/util/history.kt
@@ -6,6 +6,10 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
 
 
 val KEY_LIST = stringPreferencesKey("list")
@@ -13,41 +17,44 @@ val KEY_LIST = stringPreferencesKey("list")
 val Context.ds: DataStore<Preferences> by preferencesDataStore(name = "history")
 
 suspend fun Context.addHistory(result: String): List<String> {
-    // val value = getHistories().apply {
-    //     if (contains(result)) {
-    //         removeAt(indexOf(result))
-    //     }
-    //     if (size > 20) {
-    //         removeFirstOrNull()
-    //     }
-    //
-    //     if (result == lastOrNull()) return this
-    //     add(result)
-    // }
-    //
-    // ds.edit { settings ->
-    //     settings[KEY_LIST] = Json.encodeToString(value)
-    // }
-    // return value
-    return emptyList()
+    val value = getHistories().apply {
+        if (result.isBlank()) {
+            return this
+        }
+        if (contains(result)) {
+            removeAt(indexOf(result))
+        }
+        if (size > 20) {
+            removeFirstOrNull()
+        }
+
+        if (result == lastOrNull()) return this
+        add(result)
+    }
+
+    if (result.isNotBlank()) {
+        ds.edit { settings ->
+            settings[KEY_LIST] = Json.encodeToString(value)
+        }
+    }
+    return value
 }
 
 private fun Context.getHistories(): MutableList<String> {
-    // var json = "[]"
-    //
-    // runBlocking {
-    //     ds.data.map { settings ->
-    //         val listString = settings[KEY_LIST] ?: return@map
-    //         json = listString
-    //     }.first()
-    // }
-    // return try {
-    //     Json.decodeFromString(json)
-    // } catch (e: Exception) {
-    //     e.printStackTrace()
-    //     mutableListOf()
-    // }
-    return mutableListOf()
+    var json = "[]"
+
+    runBlocking {
+        ds.data.map { settings ->
+            val listString = settings[KEY_LIST] ?: return@map
+            json = listString
+        }.first()
+    }
+    return try {
+        Json.decodeFromString(json)
+    } catch (e: Exception) {
+        e.printStackTrace()
+        mutableListOf()
+    }
 }
 
 suspend fun Context.clearHistory() {
